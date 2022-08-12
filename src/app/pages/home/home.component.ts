c177a751948d0d85bc27459d70c9ac91e3dd1cc3
DH7789-dev <dharnaud77@hotmail.fr>
2022-08-12 16:57:35 +0200

    fix typeWritter

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// @ts-ignore
+import typeWriter from 't-writer.js';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,7 +16,39 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.writer1(typeWriter);
   }
+
+
+  // tslint:disable-next-line:no-shadowed-variable typedef
+  writer1(typeWriter: any) {
+    const target = document.querySelector('.tw1');
+
+    const writer1 = new typeWriter(target, {
+      loop: true,
+      typeSpeed: 50,
+      deleteSpeed: 30,
+      typeColor: '#fff'
+    });
+
+    writer1
+      .type('Show me the code.')
+      .rest(1500)
+      .remove(7)
+      .rest(200)
+      .clear()
+      .type('Discovered technologies.')
+      .rest(1500)
+      .remove(13)
+      .rest(200)
+      .clear()
+      .type('Discovered all project.')
+      .rest(2000)
+      .clear()
+      .start();
+  }
+
+
 
 
   changeText(nameChange: string): void{
